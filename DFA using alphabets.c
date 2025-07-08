f4a487c492ef1cf546289325471d89f24ce2acf5
Gopikrish2511j <gopikrishnareddym2055.sse@saveetha.com>
2025-07-08 13:31:45 +0530

    Add files via upload

---
+++ DFA using alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	int len;
+	char str[100];
+	
+	printf("enter the string: ",str);
+	scanf("%s",&str);
+	
+	len = strlen(str);
+	
+	if(str > 0 && str[0] == 'a' && str[len-1] =='a'){
+		printf("Accepted");
+	}else{
+		printf("Rejected");
+	}
+	
+	return 0;
+}
